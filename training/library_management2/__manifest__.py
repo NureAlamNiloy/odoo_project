4eb3003371b5a8de4cad717f9196b935f8d48761
NureAlamNiloy <mdniloy74779@gmail.com>
2025-04-22 11:10:08 +0600

    Niloy

---
+++ training/library_management2/__manifest__.py
@@ -0,0 +1,21 @@
+
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Library Management System',
+    'version': '1.1',
+    'category': 'niloy',
+    'sequence': -95,
+    'summary': 'Library Management System.. This is for my day01 task',
+    'website': 'https://www.linkedin.com/in/NureAlamNiloy',
+    'depends': [
+        'base_setup',
+    ],
+    'data': [ 
+
+    ],
+    'installable': True,
+    'application': True,
+    # 'license': 'LGPL-4',
+}
